d8979c1d4c7937ae275bbf6f0bfadda90027d4ed
ErikBurdett <erikaburdet@gmail.com>
2021-04-05 18:25:43 -0500

    finished except clear all button throws and error. Also need to use preventDefault (i think) to stop from losing data on reload.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,88 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Navbar} from './components/Navbar';
+import {TodoRows} from './components/TodoRows';
+import {TodoList} from './components/TodoList';
 
-class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
-  render() {
-    return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
-      </div>
-    );
+export default class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			userName: 'Telephone Heater',
+			todoItems: [
+				{ action: 'Track Song', done: true },
+				{ action: 'Export & Design Cover', done: false },
+				{ action: 'Upload & Share', done: false },
+        { action: 'Add Track to Album Folder', done: false },
+        
+			],
+			newTodo: '',
+		};
+	}
+
+	updateValue = (event) => {
+		this.setState({ newTodo: event.target.value });
+	};
+
+	newTodo = () => {
+		this.setState({
+			todoItems: [
+				...this.state.todoItems,
+				{ action: this.state.newTodo, done: false },
+			],
+		});
+	};
+
+	todoRows = () =>
+		this.state.todoItems.map((item) => (
+			<TodoRows key={item.action} item={item} callback={this.toggleDone} />
+		));
+
+	toggleDone = (todo) =>
+		this.setState({
+			todoItems: this.state.todoItems.map((item) =>
+				item.action === todo.action ? { ...item, done: !item.done } : item
+			),
+		});
+
+  // function to clear all done items
+  clearDone = e =>{
+    this.setState({
+      todoItems: this.state.todoItems.filter(item=>
+        (!item.done))
+      });
   }
-}
 
-export default App;
+	render = () => (
+		<div className="container">
+			<div className="row">
+				<Navbar name={this.state.userName} />
+				<div className="col-12">
+					<input
+						className="form-control"
+						value={this.state.newToDo}
+						onChange={this.updateValue}
+					/>
+					<button className="btn btn-primary" onClick={this.newTodo}>
+						Add
+					</button>
+				</div>
+				<div className="col-12">
+					<table className="table">
+						<thead>
+							<tr>
+								<th>Task</th>
+								<th>Complete</th>
+							</tr>
+						</thead>
+						<tbody>{this.todoRows()}</tbody>
+					</table>
+				</div>
+			</div>
+      <TodoList clearDone={this.clearDone}/>
+      {/* <button className="btn btn-primary" onClick={handleClick}>
+        Clear Complete Tasks
+      </button> */}
+		</div>
+	);
+}
